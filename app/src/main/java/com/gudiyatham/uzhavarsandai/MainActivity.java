83f58b67d22536767336626ad00efa363cc41bbe
creaivecoder567 <sarathcreativethinker@gmail.com>
2017-11-21 20:21:40 +0530

    Fragment is created.

--- app/src/main/java/com/gudiyatham/uzhavarsandai/MainActivity.java
+++ app/src/main/java/com/gudiyatham/uzhavarsandai/MainActivity.java
@@ -3,6 +3,8 @@ package com.gudiyatham.uzhavarsandai;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,6 +19,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.gudiyatham.uzhavarsandai.adapter.CategoryAdapter;
+import com.gudiyatham.uzhavarsandai.fragment.AboutFragment;
+import com.gudiyatham.uzhavarsandai.fragment.HomeFragment;
 import com.gudiyatham.uzhavarsandai.model.Product;
 
 import java.util.ArrayList;
@@ -24,11 +28,13 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    RecyclerView recyclerView;
-    private CategoryAdapter categoryAdapter;
-    ArrayList<Product> arrayList = new ArrayList<>();
 
-    DatabaseReference databaseProducts;
+
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+    private HomeFragment mainFragment;
+
+
     //DatabaseReference databaseItems;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,9 +43,21 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    mainFragment = new HomeFragment();
+                    Bundle bundle = new Bundle();
+                    mFragmentManager = getSupportFragmentManager();
+                    mFragmentTransaction = mFragmentManager.beginTransaction();
+                    mainFragment.setArguments(bundle);
+                    mFragmentTransaction.replace(R.id.containerView, mainFragment).commit();
                    // mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
+                    AboutFragment about = new AboutFragment();
+                    //Bundle bundle = new Bundle();
+                    mFragmentManager = getSupportFragmentManager();
+                    mFragmentTransaction = mFragmentManager.beginTransaction();
+                    //mainFragment.setArguments(bundle);
+                    mFragmentTransaction.replace(R.id.containerView, about).commit();
                    // mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
@@ -55,70 +73,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+
         //databaseItems = FirebaseDatabase.getInstance().getReference("items");
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        recyclerView = findViewById(R.id.recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        categoryAdapter= new CategoryAdapter(arrayList,this);
-        recyclerView.setAdapter(categoryAdapter);
+        mainFragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mainFragment.setArguments(bundle);
+        mFragmentTransaction.replace(R.id.containerView, mainFragment).commit();
     }
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        //attaching value event listener
-        databaseProducts.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                //clearing the previous artist list
-                arrayList.clear();
-                Log.d("product", "onDataChange: "+dataSnapshot);
-                //iterating through all the nodes
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    //getting artist
-                   Product product = postSnapshot.getValue(Product.class);
-                    //Product product = postSnapshot.child("name").getValue(Product.class);
-                    Log.d("product", "onDataChange: "+product.toString());
-                    Toast.makeText(MainActivity.this, ""+product.toString(), Toast.LENGTH_SHORT).show();
-                    //adding artist to the list
-                    arrayList.add(product);
-                }
-
-                //creating adapter
-              //  ArtistList artistAdapter = new ArtistList(MainActivity.this, artists);
-                //attaching adapter to the listview
-              //  listViewArtists.setAdapter(artistAdapter);
-
-                categoryAdapter.notifyDataSetChanged();
-            }
-
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.d("error", "onCancelled: "+databaseError);
-            }
-        });
-
-//        databaseItems.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//              //Product product =  dataSnapshot.getValue(Product.class);
-//                //Log.d("product", "onDataChange: "+product.getName());
-//                String name= dataSnapshot.child("name").getValue(String.class);
-//                Log.d("product", "onDataChange: "+name);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-    }
 
 }
---
+++ app/src/main/java/com/gudiyatham/uzhavarsandai/fragment/AboutFragment.java
@@ -0,0 +1,112 @@
+package com.gudiyatham.uzhavarsandai.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.gudiyatham.uzhavarsandai.MainActivity;
+import com.gudiyatham.uzhavarsandai.R;
+import com.gudiyatham.uzhavarsandai.adapter.CategoryAdapter;
+import com.gudiyatham.uzhavarsandai.model.Product;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+
+
+    RecyclerView recyclerView;
+    private CategoryAdapter categoryAdapter;
+    ArrayList<Product> arrayList = new ArrayList<>();
+
+    DatabaseReference databaseProducts;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = v.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        categoryAdapter= new CategoryAdapter(arrayList,getContext());
+        recyclerView.setAdapter(categoryAdapter);
+        return v;
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        //attaching value event listener
+        databaseProducts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //clearing the previous artist list
+                arrayList.clear();
+                Log.d("product", "onDataChange: "+dataSnapshot);
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting artist
+                    Product product = postSnapshot.getValue(Product.class);
+                    //Product product = postSnapshot.child("name").getValue(Product.class);
+                    Log.d("product", "onDataChange: "+product.toString());
+                    // Toast.makeText(MainActivity.this, ""+product.toString(), Toast.LENGTH_SHORT).show();
+                    //adding artist to the list
+                    arrayList.add(product);
+                }
+
+                //creating adapter
+                //  ArtistList artistAdapter = new ArtistList(MainActivity.this, artists);
+                //attaching adapter to the listview
+                //  listViewArtists.setAdapter(artistAdapter);
+
+                categoryAdapter.notifyDataSetChanged();
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("error", "onCancelled: "+databaseError);
+            }
+        });
+
+//        databaseItems.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//              //Product product =  dataSnapshot.getValue(Product.class);
+//                //Log.d("product", "onDataChange: "+product.getName());
+//                String name= dataSnapshot.child("name").getValue(String.class);
+//                Log.d("product", "onDataChange: "+name);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+    }
+}
